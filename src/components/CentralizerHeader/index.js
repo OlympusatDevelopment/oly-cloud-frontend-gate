f65d8906794e904e7592101a83d3404f021b3b92
Adam Gedney <adam@olympussat.com>
2018-06-25 15:39:31 -0400

    CONVERTED header to scss

--- src/components/CentralizerHeader/index.js
+++ src/components/CentralizerHeader/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styles from './styles';
 import './style.scss';
 
 export class CentralizerHeader extends Component {
@@ -7,21 +6,21 @@ export class CentralizerHeader extends Component {
     const { options, user } = this.props; 
    
     return (
-      <div className="olyauth__centralizerHeader" style={styles.header}>
-        <div style={Object.assign({}, styles.column, styles.columnFirst)} >
-          <div style={styles.headerGravatar} className="olyauth__centralizerHeaderGravatar">
-            <img style={styles.headerGravatarImg} src={user.profileImage} alt={user.name} />
+      <div className="olyauth__centralizerHeader">
+        <div>
+          <div className="olyauth__centralizerHeaderGravatar">
+            <img src={user.profileImage} alt={user.name} />
           </div>
         </div>
-        <div style={styles.column} >
-          <p style={styles.headerText}>
-            <span style={styles.headerName} className="bold" title={user.email}>{user.name} {user.lastname}</span>
+        <div>
+          <p>
+            <span className="bold" title={user.email}>{user.name} {user.lastname}</span>
           </p>
-          <p style={styles.headerText}>
+          <p>
             {(user.organization ? user.organization.name : '')}
           </p>
-          <p style={styles.headerText}>
-            <span style={styles.headerUsername} title={user.email}>{user.email}</span>
+          <p>
+            <span title={user.email}>{user.email}</span>
           </p>
         </div>
       </div>
