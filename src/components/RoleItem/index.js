bdbbc271a49a5ad4ede3151970bf92dadd7e7931
Adam Gedney <adam@olympussat.com>
2018-06-26 09:05:46 -0400

    CONVERTED all components to scss

--- src/components/RoleItem/index.js
+++ src/components/RoleItem/index.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import styles from './styles'; 
+import React, { Component } from 'react'; 
 import './style.scss';
  
 export class RoleItem extends Component {
   render() {
     const { role, onItemClick, activeRole } = this.props;
     return ( 
-      <li className="olyauth__centralizerRoleItem" style={styles.li} onClick={onItemClick}>
-        <div style={Object.assign({},styles.column, styles.columnFirst)}>
-          <img style={styles.img} src={role.organization.logo} alt={role.organization.name}/>
+      <li className="olyauth__centralizerRoleItem" onClick={onItemClick}>
+        <div>
+          <img src={role.organization.logo} alt={role.organization.name}/>
         </div>
-        <div style={styles.column}>
-          <p style={styles.columnP} className="bold">{role.name}</p>
-          <p style={Object.assign({},styles.columnP, styles.columnOrg)} >{role.organization.name}</p>
+        <div>
+          <p className="bold">{role.name}</p>
+          <p>{role.organization.name}</p>
         </div>
       </li>
     )
--- src/components/RoleSelector/index.js
+++ src/components/RoleSelector/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import styles from './styles';
 import { RoleItem } from '../RoleItem';
 import './style.scss';
 
@@ -11,13 +10,12 @@ export class RoleSelector extends Component {
   }
 
   render() {
-    const { roles } = this.props
     return (
-      <div className="olyauth__roleSelector" style={styles.olyauth__roleSelector}>
-        <div className="olyauth__roleSelectorInner" style={styles.olyauth__roleSelectorInner}>
-        <ul style={styles.ul}>
-          { roles.map(role => (<RoleItem key={role.id} role={role} onItemClick={this.onItemClick}/>)) }
-        </ul>
+      <div className="olyauth__roleSelector">
+        <div className="olyauth__roleSelectorInner">
+          <ul>
+            { this.props.roles.map(role => (<RoleItem key={role.id} role={role} onItemClick={this.onItemClick}/>)) }
+          </ul>
         </div>
       </div>
     )
