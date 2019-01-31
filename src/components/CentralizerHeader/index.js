0bffd839e1671343d0243d20507a30b234951614
Adam Gedney <adam@olympussat.com>
2019-01-31 16:11:03 -0500

    UPDATED header gravatar

--- src/components/CentralizerHeader/index.js
+++ src/components/CentralizerHeader/index.js
@@ -5,17 +5,25 @@ import { HeaderUtils} from '../HeaderUtils';
 export class CentralizerHeader extends Component {
   render() { 
     const { options, user } = this.props; 
-   
+    user.profileImage = false;
+    const noProfileImg = !user.profileImage || (user.profileImage || '').indexOf('default_profile.jpg') > -1;
+    const profileImg = noProfileImg 
+      ? (<p style={{ background: options.brandingColor }}>{user.email.charAt(0).toUpperCase()}</p>) 
+      : (<img src={`${options.assetsBucketUrl}${user.profileImage}`} alt={user.name} />);
+    const gravatarClassnames = noProfileImg 
+      ? 'olyauth__centralizerHeaderGravatar olyauth__centralizerHeaderGravatar--cssGravatar' 
+      : 'olyauth__centralizerHeaderGravatar';
+
     return (
       <div className="olyauth__centralizerHeader">
         <div>
-          <div className="olyauth__centralizerHeaderGravatar">
-            <img src={`${options.assetsBucketUrl}${user.profileImage}`} alt={user.name} />
+          <div className={gravatarClassnames}>
+            {profileImg}
           </div>
         </div>
         <div>
           <p>
-            <span className="bold" title={`${user.name} ${user.lastname}`}>{user.name ? `${user.name} ${user.lastname}` : ''}</span>
+            <span className="bold" title={`${user.name} ${user.lastname || ''}`}>{user.name ? `${user.name} ${user.lastname || ''}` : ''}</span>
           </p>
           {user.organization && 
             (<p>user.organization.name</p>)
